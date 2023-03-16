4920786f6cdb8b765b03b7d187346a79ce902736
Eliaslindh <ellilindh22@gmail.com>
2023-03-16 12:55:36 +0200

    commit

--- positiv negatuív/app.js
+++ positiv negatuív/app.js
@@ -1,22 +1,18 @@
-const div = document.querySelector('#div')
-const input = document.querySelector('#input')
+let parent = document.querySelector('#parent')
 
-
-
-function checkNumber() {
-    if (input.value > 0) {
-        div.append('positiv')
-        console.log("positiv");
+document.querySelector('button').addEventListener('click', () => {
+    let input = document.querySelector('input').value
+    if (input > 0) {
+        parent.innerHTML = `<h1 class="green">Detta är positiv</h1>`
+        document.getElementById('h1').style.color = "green"
+    } else if (input == 0) {
+        parent.innerHTML = `<h1 class="blue">Detta är noll</h1>`
+        document.getElementById('h1').style.color = "blue"
+    } else {
+        parent.innerHTML = `<h1 class="red">Detta är negativ</h1>`
+        document.getElementById('h1').style.color = "red"
     }
-    else if (input.value < 0) {
-        div.append('Negativ')
-        console.log('negativ')
-    }
-    else {
-        div.append('Noll')
-        console.log('noll')
-    }
-}
+})
 
 
 
